5519a6db4f182f759ba673b7dca6117746ecc691
futurum-dev <me@futurum.dev>
2023-12-05 23:31:55 +0000

    Upgrade to dotnet 8 and nugets

--- src/Futurum.WebApiEndpoint.Micro/WebApiResultsExtensions.ToOk.cs
+++ src/Futurum.WebApiEndpoint.Micro/WebApiResultsExtensions.ToOk.cs
@@ -3,6 +3,12 @@ namespace Futurum.WebApiEndpoint.Micro;
 public static partial class WebApiResultsExtensions
 {
     /// <summary>
+    /// Transforms to a <see cref="Ok"/>.
+    /// </summary>
+    public static Ok ToOk(HttpContext context) =>
+        TypedResults.Ok();
+
+    /// <summary>
     /// Transforms a <typeparamref name="T"></typeparamref> to a <see cref="Ok{T}"/>.
     /// </summary>
     public static Ok<T> ToOk<T>(this T value) =>
