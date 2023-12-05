61e3cf765f1748090d01cf799950067f8b86715a
futurum-dev <me@futurum.dev>
2023-12-05 20:51:01 +0000

    Upgrade to dotnet 8 and nugets

--- src/Futurum.WebApiEndpoint.Micro.Generator/DiagnosticDescriptors.cs
+++ src/Futurum.WebApiEndpoint.Micro.Generator/DiagnosticDescriptors.cs
@@ -7,8 +7,8 @@ public static class DiagnosticDescriptors
     public static readonly DiagnosticDescriptor WebApiEndpointNonEmptyConstructor = new(
         "FWAEM0001",
         "Non empty constructor found on WebApiEndpoint",
-        $"WebApiEndpoint class '{{0}}' does not have an empty constructor.{Environment.NewLine}" +
-        $"We recommend that WebApiEndpoint's have an empty constructor and to take any injectable dependencies as parameters via the minimal API method itself.{Environment.NewLine}" +
+        $"WebApiEndpoint class '{{0}}' does not have an empty constructor.\n" +
+        $"We recommend that WebApiEndpoint's have an empty constructor and to take any injectable dependencies as parameters via the minimal API method itself.\n" +
         $"Constructor dependencies will have a lifetime outside of the minimal API lifetime and could have unintended consequences.",
         "Futurum.WebApiEndpoint.Micro.Generator",
         DiagnosticSeverity.Warning,
